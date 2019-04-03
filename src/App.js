fdfb0e034c962305712dd37a4b4d30225e82ef74
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-04-03 12:49:30 -0400

    Initial commit - Create React App + React p5 Wrapper (initial display w/ copied code)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import P5Wrapper from './react-p5-wrapper';
+import sketch from './sketch';
+import sketch2 from './sketch2';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			rotation: 150,
+			stateSketch: sketch,
+		};
+	}
+
+	rotationChange(e){
+		this.setState({rotation:e.target.value});
+	}
+
+	pressEvent(){
+		this.state.stateSketch === sketch ? this.setState({stateSketch:sketch2}) : this.setState({stateSketch:sketch});
+	}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <P5Wrapper sketch={this.state.stateSketch} rotation={this.state.rotation}/>
+        <input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
+        <button onClick={this.pressEvent.bind(this)}>Change Sketch</button>
       </div>
     );
   }

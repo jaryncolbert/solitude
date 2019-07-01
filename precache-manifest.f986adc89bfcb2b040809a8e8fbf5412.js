748b62e7b6b69238571300ad70478743dbf40281
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-07-01 12:21:08 -0400

    Updates

---
+++ precache-manifest.f986adc89bfcb2b040809a8e8fbf5412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631465c3c63dc68464c8be22de0d1d5a",
+    "url": "/sol-itude/index.html"
+  },
+  {
+    "revision": "eb19bdd41d5f3a409982",
+    "url": "/sol-itude/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "b6897dbab70f9952b56b",
+    "url": "/sol-itude/static/css/main.fbeee0ce.chunk.css"
+  },
+  {
+    "revision": "eb19bdd41d5f3a409982",
+    "url": "/sol-itude/static/js/2.39e684c1.chunk.js"
+  },
+  {
+    "revision": "b6897dbab70f9952b56b",
+    "url": "/sol-itude/static/js/main.a7553873.chunk.js"
+  },
+  {
+    "revision": "d12e3d0e137752da1a18",
+    "url": "/sol-itude/static/js/runtime~main.edc8ef9a.js"
+  },
+  {
+    "revision": "a9fcb9bcdbd0c574cd16c59823499f90",
+    "url": "/sol-itude/static/media/sol_lewitt_391_color.a9fcb9bc.svg"
+  }
+]);
